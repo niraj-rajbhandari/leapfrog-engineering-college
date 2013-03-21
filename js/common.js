a9a21229b43ad2b9e18e24fe201d8cd6574227ab
sudip_pudasaini@hotmail.com <sudip_pudasaini@hotmail.com>
2013-03-21 11:48:33 +0545

    all file being committed

--- js/common.js
+++ js/common.js
@@ -1,44 +1,54 @@
-$(document).ready(function(){
-    var nav=$('.header');
-    
-    $(window).scroll(function(){
-        if($(this).scrollTop()>65){
+$(document).ready(function() {
+    var nav = $('.header');
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 65) {
             nav.addClass("header-scrolled");
         }
         else
-            {
-                nav.removeClass("header-scrolled");
-            }
+        {
+            nav.removeClass("header-scrolled");
+        }
     });
 
 });
-function changeContent(){
-   document.getElementById('One').style.display = 'block'; 
-     
-    }
+//function changeContent(){
+//   document.getElementById('One').style.display = 'block'; 
+//     
+//    }
+//
+//function loadNull(){
+//   document.getElementById('One').style.display = 'none';  
+//}
+//function changeContentShow(){
+//   document.getElementById('Two').style.display = 'block'; 
+//     
+//    }
+//
+//function loadNullValue(){
+//   document.getElementById('Two').style.display = 'none';  
+//}
+//function changeContentShowAgain(){
+//   document.getElementById('Three').style.display = 'block'; 
+//     
+//    }
+//
+//function loadNullValueAgain(){
+//   document.getElementById('Three').style.display = 'none';  
+//}
+//function loadInitial(){
+//    document.getElementById('One').style.display = 'none';  
+//     document.getElementById('Two').style.display = 'none'; 
+//      document.getElementById('Three').style.display = 'none';
+//     
+//}
+$(document).ready(function() {
+    $('#showhide').toggle(function() {
+        $('#showhide').text('ShowMore');
+        $('#One').hide();
+    }, function() {
+        $('#showhide').text('Hide');
+        $('#One').show();
+    });
 
-function loadNull(){
-   document.getElementById('One').style.display = 'none';  
-}
-function changeContentShow(){
-   document.getElementById('Two').style.display = 'block'; 
-     
-    }
-
-function loadNullValue(){
-   document.getElementById('Two').style.display = 'none';  
-}
-function changeContentShowAgain(){
-   document.getElementById('Three').style.display = 'block'; 
-     
-    }
-
-function loadNullValueAgain(){
-   document.getElementById('Three').style.display = 'none';  
-}
-function loadInitial(){
-    document.getElementById('One').style.display = 'none';  
-     document.getElementById('Two').style.display = 'none'; 
-      document.getElementById('Three').style.display = 'none';
-     
-}
+});
